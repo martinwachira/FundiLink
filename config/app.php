8a4302a6187cf62084c6122a00336363f32b5f18
martin-wachira <martinnwachira@gmail.com>
2020-09-03 22:12:53 +0300

    rating provider added

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // ratings provider
+        willvincent\Rateable\RateableServiceProvider::class,
 
     ],
 
