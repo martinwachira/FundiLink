b82a8c80864e8a6cbec52416e514a76856b9da6d
martin-wachira <martinnwachira@gmail.com>
2020-09-03 21:57:21 +0300

    setup ratings for users

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // ratings provider
+        willvincent\Rateable\RateableServiceProvider::class,
+
     ],
 
     /*
